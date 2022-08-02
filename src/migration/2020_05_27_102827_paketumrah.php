b70fe9731c8334dac37afab231bcf5e38f025988
Adnan Bayu <adnan.bayufirmansyah@gmail.com>
2022-08-02 18:04:32 +0700

    update class name migrasi

--- src/migration/2020_05_27_102827_paketumrah.php
+++ src/migration/2020_05_27_102827_paketumrah.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class PaketUmrah extends Migration
+class PaketUmrahBatch1 extends Migration
 {
     /**
      * Run the migrations.
